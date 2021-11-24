5c90e062be25b0a54109b5010f884f09d2e8bdf3
Raphael Benoliel <raphael2gb@gmail.com>
2021-11-24 23:32:02 +0200

    com

--- gitde/main.c
+++ gitde/main.c
@@ -5,10 +5,4 @@
 //  Created by Raphael Benoliel on 24/11/2021.
 //
 
-#include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
-}
+#include "main.h"
